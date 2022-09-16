8566b7397a7589b43e998b751cd8513138143894
jonas <jonas.skott.nielsen@live.dk>
2022-09-16 15:47:35 +0200

    added Button and Stack

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,8 @@
+import Button from '../components/Button';
+
+export default {
+	title: "Button",
+	component: Button
+}
+
+export const Red = () => <Button label="Pres2 me" backgroundColor='blue' />
